d67d2216ad937b6bff250fdfa7d1dd02594301b3
Felipe <pufi2kk@gmail.com>
2026-03-27 16:09:11 -0300

    habilidades parcialmnte feitas

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+class habilidade:
+
+    def __init__(self, master, nome):
+        self.nome = nome
+        self.master = master
+
+        self.frameHabilidade = ttk.Frame(master, padding = 15, relief="solid", width= 100)
+        self.frameHabilidade.grid()
+        
+        self.barra = ttk.Progressbar(self.frameHabilidade, orient= "horizontal", length= 300, mode = "determinate", maximum=30)
+        self.barra.pack(side='right')
+
+        self.botao = ttk.Button(self.frameHabilidade, text= self.nome, command= self.iniciar_progresso)
+        self.botao.pack(side='left')
+    
+    def iniciar_progresso(self):
+        self.barra["value"] = 0
+        self.incrementar()
+
+    def incrementar(self):
+        if self.barra["value"] < self.barra["maximum"]:
+            self.barra["value"] += 1
+            # Usamos o self.master (que é o root) para chamar o after
+            self.master.after(50, self.incrementar)
+        else:
+            self.barra["value"] = 0
+            self.comando_final()
+
+    def comando_final(self):
+        messagebox.showinfo("Fim", "O comando foi executado com sucesso!")
+
+root = Tk()
+root.title("simulador de farmar aura")
+root.geometry("500x500")
+
+aura = IntVar(root, value = 0)
+
+# Header
+frame1 = ttk.Frame(root,width=500, height=100, relief="solid")
+frame1.pack(pady=20, padx=20, fill="both", expand=False)
+
+contadorAura = ttk.Label(frame1, text="0", font=("Comic Sans MS", 20))
+contadorAura.place(relx=1.0, rely=0, anchor='ne')
+
+# frame das habilidades
+frame2 = ttk.Frame(root, padding = 20, relief="solid", width=500)
+frame2.pack()
+
+habilidade1 = habilidade(frame2, "tocar phonk em publico")
+habilidade2 = habilidade(frame2, "moggar 1 beta")
+
+root.mainloop()
