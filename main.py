e40a35dd4d46b5700c4159c0d7cea2f15b08101e
um-drade <168025272+um-drade@users.noreply.github.com>
2026-03-31 08:16:19 -0300

    habilidades nao podem ser ativadas se ja estiverem ativas

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from tkinter import messagebox
 
 class habilidade:
 
+    emProgresso = False
+    
     def __init__(self, master, nome, ganho, tempoEspera):
         """
+        master: pai do widget
+        nome: nome da habilidade
+        ganho: quantidade de aura ganha com esta habilidade
         tempoEspera: tempo em décimos de segundo para a duração da barra da habilidade
         """
 
@@ -24,14 +29,17 @@ class habilidade:
         self.botao.pack(padx= 5, side='left')
     
     def iniciar_progresso(self):
-        self.barra["value"] = 0
-        self.incrementar()
+        if not self.emProgresso:
+            self.emProgresso = True
+            self.barra["value"] = 0
+            self.incrementar()
 
     def incrementar(self):
         if self.barra["value"] < self.barra["maximum"]:
             self.barra["value"] += 1
             self.master.after(50, self.incrementar)
         else:
+            self.emProgresso = False
             self.barra["value"] = 0
             self.comando_final()
 
@@ -49,7 +57,7 @@ aura = IntVar(root, value = 0)
 
 # Header
 frame1 = ttk.Frame(root,width=500, height=100, relief="solid")
-frame1.pack(pady=20, padx=20, fill="both", expand=False)
+frame1.pack(pady=20, padx=20, fill="x", expand=False)
 
 contadorAura = ttk.Label(frame1, text="0", font=("Comic Sans MS", 20))
 contadorAura.place(relx=1.0, rely=0, anchor='ne')
@@ -64,7 +72,10 @@ aura.trace_add("write", AtualizarLabelAura)
 frame2 = ttk.Frame(root, padding = 20, relief="solid", width=500)
 frame2.pack()
 
-habilidade1 = habilidade(master= frame2, nome="moggar 1 beta", ganho= 1, tempoEspera= 30)
-habilidade2 = habilidade(master= frame2, nome= "tocar phonk \nem publico", ganho= 3, tempoEspera= 50)
+habilidade1 = habilidade(master= frame2, nome="moggar 1 beta", ganho= 1, tempoEspera= 20)
+
+habilidade2 = habilidade(master= frame2, nome= "tocar phonk \nem publico", ganho= 3, tempoEspera= 40)
+
+habilidade3 = habilidade(master= frame2, nome= "botar gelo \nagachado", ganho= 6, tempoEspera= 60)
 
 root.mainloop()
